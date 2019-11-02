2b74f1e928cfc5e4e7e502b4ff9c444fae035a66
altzone <root@localhost.localdomain>
2019-11-02 18:08:00 +0100

    - Change Mysql class - remove Cli class - code restructuring in object mode

--- lib/Db.php
+++ lib/Db.php
@@ -3,64 +3,121 @@
 namespace phpBorg;
 
 use Memcached;
-
-class Db
+use mysqli;
+class Db extends Core
 {
-    /**
-     * @param $server
-     * @return false|\mysqli
-     */
-    function db_connect($server) {
-        switch ($server) {
-            case "backup":
-                $link = mysqli_connect('127.0.0.1', 'user', 'pass', 'db');
-                return $link;
-                break;
-        }
-        return false;
-    }
+    protected $connection;
+    protected $query;
+    public    $query_count = 0;
+    public    $sql_err = 1;
 
-    /**
-     * @param $reqsql
-     * @param $db
-     * @param int $cache
-     * @return bool|mixed|\mysqli_result
-     */
-    function fsql ($reqsql, $db, $cache = 0){
-        $mem = new Memcached();
-        $mem->addServer("127.0.0.1", 11211);
-
-        $querykey = "KEY" . md5($reqsql);
-        $result = $mem->get($querykey);
-
-        if (($mem->getResultCode() == Memcached::RES_NOTFOUND) || $cache == 0) {
-            $rez = mysqli_query($db, $reqsql) or die('MySQL error: <br>' . $reqsql . '<br>'. mysqli_error($db));
-            $mem->set($querykey, $rez, $cache);
-        } else {
-            $rez = $result;
+        public function __construct($dbhost = '10.10.30.60', $dbuser = 'backup', $dbpass = 'QSDJSQDKJSQDJK34434', $dbname = 'backup', $charset = 'utf8') {
+                $this->connection = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+                if ($this->connection->connect_error) {
+                        die('Echec de la connexion - ' . $this->connection->connect_error);
+                }
+                $this->connection->set_charset($charset);
         }
 
-        return $rez;
+        public function query($query) {
+                if ($this->query = $this->connection->prepare($query)) {
+                        if (func_num_args() > 1) {
+                                $x = func_get_args();
+                                $args = array_slice($x, 1);
+                                $types = '';
+                                $args_ref = array();
+                                foreach ($args as $k => &$arg) {
+                                        if (is_array($args[$k])) {
+                                                foreach ($args[$k] as $j => &$a) {
+                                                        $types .= $this->_gettype($args[$k][$j]);
+                                                        $args_ref[] = &$a;
+                                                }
+                                        } else {
+                                                $types .= $this->_gettype($args[$k]);
+                                                $args_ref[] = &$arg;
+                                        }
+                                }
+                                array_unshift($args_ref, $types);
+                                call_user_func_array(array($this->query, 'bind_param'), $args_ref);
+                        }
+                        $this->query->execute();
+                        if ($this->query->errno) {
+                                printf("Échec de la requete : %s\n", $this->query->error);
+                                if ($this->sql_err) die();
+                        }
+                        $this->query_count++;
+                } else {
+                        printf("erreur dans la requete: %s\n", $this->connection->error);
+                        if ($this->sql_err) die();
+                }
+                return $this;
     }
 
-    /**
-     * @param $reqsql
-     * @param $db
-     * @param int $cache
-     * @return mixed|object|null
-     */
-    function fsql_object ($reqsql, $db, $cache = 0) {
-        $mem = new Memcached();
-        $mem->addServer("127.0.0.1", 11211);
-
-        $querykey = "KEY" . md5($reqsql);
-        $result = $mem->get($querykey);
-        if (($mem->getResultCode() == Memcached::RES_NOTFOUND) || $cache == 0 ) {
-            $sql = mysqli_query($db, $reqsql) or die('MySQL error: <br>' . $reqsql . '<br>' . mysqli_error($db));
-            $result = mysqli_fetch_object($sql);
-            $mem->set($querykey, $result, $cache);
+        public function fetchAll() {
+            $params = array();
+            $meta = $this->query->result_metadata();
+            while ($field = $meta->fetch_field()) {
+                $params[] = &$row[$field->name];
+            }
+            call_user_func_array(array($this->query, 'bind_result'), $params);
+        $result = array();
+        while ($this->query->fetch()) {
+            $r = array();
+            foreach ($row as $key => $val) {
+                $r[$key] = $val;
+            }
+            $result[] = $r;
+        }
+        $this->query->close();
+                return $result;
         }
 
-        return $result;
-    }
+        public function fetchArray() {
+            $params = array();
+            $meta = $this->query->result_metadata();
+            while ($field = $meta->fetch_field()) {
+                $params[] = &$row[$field->name];
+            }
+            call_user_func_array(array($this->query, 'bind_result'), $params);
+        $result = array();
+                while ($this->query->fetch()) {
+                        foreach ($row as $key => $val) {
+                                $result[$key] = $val;
+                        }
+                }
+        $this->query->close();
+                return $result;
+        }
+
+        public function sql_error() {
+                return $this->connection->error;
+        }
+        public function numRows() {
+                $this->query->store_result();
+                return $this->query->num_rows;
+        }
+
+        public function close() {
+                return $this->connection->close();
+        }
+
+        public function affectedRows() {
+                return $this->query->affected_rows;
+	}
+
+	public function insertId() {
+		return $this->query->insert_id;
+	}
+
+        private function _gettype($var) {
+            if(is_string($var)) return 's';
+            if(is_float($var)) return 'd';
+            if(is_int($var)) return 'i';
+            return 'b';
+        }
+
+        public function sql_err ($sql_err) {
+                $this->sql_err = $sql_err;
+        }
+
 }
